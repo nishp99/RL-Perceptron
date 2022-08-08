4675cd8a2eb2e96a00231cbf41b7e58c2e827902
nshl99 <78479831+nshl99@users.noreply.github.com>
2022-08-08 18:40:31 +0100

    Update script.py

--- script.py
+++ script.py
@@ -44,5 +44,5 @@ for i in lr_1_s:
 	for j in lr_2_s:
 		for theta, w_student in students[100:101]:
 			job = executor.submit(n_or_more_neg, D = 400, teacher = w_teacher, rad = theta, student = w_student, T = 12, n = 9, lr_1 = i, lr_2 = j, steps = 20, experiment_path = experiment)
-
+			print('assigned job')
 
