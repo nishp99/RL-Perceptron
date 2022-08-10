15601c090280f023ec9be08db94bd9bb0fe9df18
nshl99 <78479831+nshl99@users.noreply.github.com>
2022-08-10 17:04:17 +0100

    Update script.py

--- script.py
+++ script.py
@@ -47,7 +47,7 @@ jobs = []
 with executor.batch():
 	for i in lr_1_s:
 		for j in lr_2_s:
-			for theta, w_student in students[:15]:
+			for theta, w_student in students[:7]:
 				job = executor.submit(n_or_more_neg, D = 400, teacher = w_teacher, rad = theta, student = w_student, T = 12, n = 9, lr_1 = i, lr_2 = j, steps = 5000, experiment_path = run_path)
 				jobs.append(job)
 
