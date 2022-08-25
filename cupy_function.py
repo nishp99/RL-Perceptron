0287f03fee7021c8a38c29774b46ed9fd2d3aa29
nshl99 <78479831+nshl99@users.noreply.github.com>
2022-08-25 15:31:07 +0100

    added testing of cupy

---
+++ cupy_function.py
@@ -0,0 +1,21 @@
+import cupy as cp
+import numpy as np
+
+def tester():
+	cp.cuda.Device(0).use()
+	x_cpu = np.array([1,2,3])
+	y_cpu = np.array([4,5,6])
+	z_cpu = x_cpu + y_cpu
+	z_c = cp.get_array_module(z_cpu)
+
+	x_gpu = cp.asarray(x_cpu)
+	y_gpu = cp.asarray(y_cpu)
+	z_gpu = x_gpu + y_gpu
+	z_g = cp.get_array_module(z_gpu)
+
+	print('number of recognised devices:')
+	print(cp.cuda.runtime.getDeviceCount())
+	print('CPU name:')
+	print(z_c.__name__)
+	print('GPU name:')
+	print(z_g.__name__)
---
+++ cupy_test_script.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cupy as cp
+import cupy_functions
+from cupy_functions import *
+import os
+import submitit
+import datetime
+
+# sys.path.append('utils')
+
+#start timestamp with unique identifier for name
+run_timestamp = datetime.datetime.now().strftime('%Y%m-%d%H-%M%S')
+#os.mkdir(with name of unique identifier)
+
+experiment_path = os.path.join('utils', "test")
+os.makedirs(experiment_path, exist_ok = True)
+
+run_path = os.path.join(experiment_path, run_timestamp)
+os.mkdir(run_path)
+
+executor = submitit.AutoExecutor(folder="utils/test")
+
+executor.update_parameters(timeout_min = 5, mem_gb = 1, gpus_per_node =1, cpus_per_task = 0, slurm_array_parallelism = 1, slurm_partition = "gpu")
+
+job = executor.submit(tester)
