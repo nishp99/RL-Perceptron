6c62115eed91e8a9b464b6652c02f615901feb9f
nshl99 <78479831+nshl99@users.noreply.github.com>
2022-08-31 17:35:31 +0100

    Update experimental_phase_script.py

--- experimental_phase_script.py
+++ experimental_phase_script.py
@@ -18,7 +18,7 @@ run_timestamp = datetime.datetime.now().strftime('%Y%m-%d%H-%M%S')
 results_path = os.path.join("utils", "results")
 os.makedirs(results_path, exist_ok = True)
 
-experiment_path = os.path.join(results_path, "n_or_more_exper_5")
+experiment_path = os.path.join(results_path, "n_or_more_exper_80")
 os.makedirs(experiment_path, exist_ok = True)
 
 run_path = os.path.join(experiment_path, run_timestamp)
@@ -27,7 +27,7 @@ os.mkdir(run_path)
 # from RL-Perceptron, utils, surface functions: generate teacher, and generate students
 
 w_teacher = gen_teacher(400)
-vectors = generate_students(w_teacher, 400, 5)
+vectors = generate_students(w_teacher, 400, 80)
 students = vectors[31:32]
 
 #s
