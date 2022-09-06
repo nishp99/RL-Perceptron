bd64b7b7fefcad90ce4a1a677dd69e2f7140a302
nshl99 <78479831+nshl99@users.noreply.github.com>
2022-09-06 12:02:27 +0100

    removed neg

--- 12_more_script.py
+++ 12_more_script.py
@@ -6,8 +6,10 @@ import scipy.special
 import math
 from utils import exper_standard_funcs
 from utils.exper_standard_funcs import *
-from utils import standard_neg_funcs
-from utils.standard_neg_funcs import *
+#from utils import standard_neg_funcs
+#from utils.standard_neg_funcs import *
+from utils import standard_funcs
+from utils.standard_funcs import *
 
 import os
 import submitit
@@ -21,7 +23,7 @@ run_timestamp = datetime.datetime.now().strftime('%Y%m-%d%H-%M%S')
 results_path = os.path.join("utils", "results")
 os.makedirs(results_path, exist_ok = True)
 
-experiment_path = os.path.join(results_path, "12_7-9-11")
+experiment_path = os.path.join(results_path, "pos_12_7-9-11")
 os.makedirs(experiment_path, exist_ok = True)
 
 run_path = os.path.join(experiment_path, run_timestamp)
@@ -42,7 +44,7 @@ executor_1.update_parameters(timeout_min = 1800, mem_gb = 4, gpus_per_node = 1, 
 jobs = []
 with executor_1.batch():
 	for n in n_s:
-		job = executor_1.submit(n_or_more_neg_exp, D = 400, teacher = w_teacher, rad = student[0], student = student[1], T = 12, n = n, lr_1 = 1, lr_2 = 0.01 , steps = 8000, experiment_path = run_path)
+		job = executor_1.submit(n_or_more_neg_exp, D = 400, teacher = w_teacher, rad = student[0], student = student[1], T = 12, n = n, lr_1 = 1, lr_2 = 0, steps = 8000, experiment_path = run_path)
 		jobs.append(job)
 
 executor_2 = submitit.AutoExecutor(folder="utils/results")
@@ -52,7 +54,7 @@ executor_2.update_parameters(timeout_min = 1800, mem_gb = 4, gpus_per_node = 0, 
 jobs_2 = []
 with executor_2.batch():
 	for n in n_s:
-		job_2 = executor_2.submit(n_or_more_neg, D = 400, teacher = w_teacher, rad = student[0], student = student[1], T = 12, n = n, lr_1 = 1, lr_2 = 0.01 , steps = 8000, experiment_path = run_path)
+		job_2 = executor_2.submit(n_or_more, D = 400, teacher = w_teacher, rad = student[0], student = student[1], T = 12, n = n, lr_1 = 1, steps = 8000, experiment_path = run_path)
 		jobs_2.append(job_2)
 
 
--- utils/standard_funcs.py
+++ utils/standard_funcs.py
@@ -200,13 +200,9 @@ def n_or_more(D, teacher, rad, student, T, n, lr_1, steps, experiment_path):
       
     step += 1
 
-  normalised_overlap = np.divide(np.copy(R),np.sqrt(np.copy(Q)))
-  theta = np.arccos(normalised_overlap)
-  P = (1- theta/np.pi)
-
-  data['p'] = P
-  data['lr'] = L_s
-  data['ang'] = rad
+  """normalised_overlap = np.divide(np.copy(R),np.sqrt(np.copy(Q)))
+        theta = np.arccos(normalised_overlap)
+        P = (1- theta/np.pi)"""
 
   file_path = os.path.join(path, 'dic.npy')
   np.save(file_path, data)
